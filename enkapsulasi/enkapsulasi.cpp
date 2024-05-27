0ea76d76b1183cdb982a8261d8d4d0d6555e3e41
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-27 10:27:30 +0700

    remote LampuRumah;

--- enkapsulasi/enkapsulasi.cpp
+++ enkapsulasi/enkapsulasi.cpp
@@ -5,10 +5,27 @@ class remote {
 private:
 	string saklarNo[10];
 public:
-	void setSaklar(int i, string value) {
+	void setSaklarNo(int i, string value) {
 		saklarNo[i] = value;
 	}
 	string getSaklarNo(int i) {
 		return saklarNo[i];
 	}
 };
+
+int main() {
+    remote LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "lampu Teras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tidur");
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+
+}
