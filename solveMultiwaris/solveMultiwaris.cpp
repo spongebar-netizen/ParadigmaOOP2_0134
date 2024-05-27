f8b8b7a782723f7b584e273610f184e69ffdd1d2
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-27 10:52:17 +0700

    class pekerja

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -12,3 +12,14 @@ public:
         cout << "orang dibuat dengan umur" << umur << "\n" << endl;
     }
 };
+
+
+class pekerja : public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
