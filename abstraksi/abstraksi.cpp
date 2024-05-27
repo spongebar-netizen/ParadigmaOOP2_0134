4e8eedb89d3a158295882ba94aa6fc4e465b6032
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-27 10:45:03 +0700

    AbstraksiKlas ak;

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
-class Abstraksiklas {
-private: string
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+    void setXY(string a, string b) {
+        x = a;
+        x = b;
+    }
+
+    void display() {
+        cout << "x,= " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
