afba999a5474287903ba1e7805a4939aefd4be79
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-27 10:51:45 +0700

    orang

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -5,4 +5,10 @@ using namespace std;
 class orang {
 public:
 	int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
 };
