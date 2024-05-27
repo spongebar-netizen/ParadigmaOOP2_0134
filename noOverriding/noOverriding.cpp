f47722943592226fd2d56fa708feca1692065e39
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-27 10:32:40 +0700

    class derivedclass

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,4 +1,16 @@
 #include<iostream>
 using namespace std;
 
-class baseClass
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class derivedclass :public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived class";
+    }
+};
